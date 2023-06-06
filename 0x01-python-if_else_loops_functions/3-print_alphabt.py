c733a84ea7b1bec5ef7cc92a0a5a98a8e325dbda
badrxd <badrxdxd@gmail.com>
2023-06-06 20:53:46 +0100

    ok

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print(''.join('{}'.format(chr(i)) for i in range(97,123) if chr(i) not in ('q', 'e') ), end='')
+for i in range(97, 123):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print('{}'.format(chr(i)), end='')
