beab2d78b045fb099dd48151bb7be27d037aacd5
badrxd <badrxdxd@gmail.com>
2023-06-06 22:14:44 +0100

    ok

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+def uppercase(str):
+    char = ""
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] <= 'z':
+            char = chr(ord(str[i]) - 32)
+        else:
+            char = str[i]
+        if i != (len(str) - 1):
+            print(char, end="")
+        else:
+            print(char)
