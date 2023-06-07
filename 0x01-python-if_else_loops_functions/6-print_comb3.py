92b869fe872d05963ede360ac88efe282ee12531
badrxd <badrxdxd@gmail.com>
2023-06-07 02:15:10 +0100

    ok

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print(', '.join('{}{}'.format(a, b) for a in range(0, 9) for b in range(a+1, 10)))
+print(', '.join('{}{}'.format(a, b) for a in range(9) for b in range(a+1, 10)))
