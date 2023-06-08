d2acf0aaccc0666a07b46f6ecd0d8ccb1c21f37b
badrxd <badrxdxd@gmail.com>
2023-06-08 19:17:45 +0100

    ok

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-      """ Simple function"""
+    """ Simple function"""
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     ls = [add, "+", sub, '-', mul, '*', div, '/']
     for i in range(0, 8, 2):
-        print("{} {} {} = {}".format(a, ls[i - 1], b, ls[i](a, b)))
+        print("{} {} {} = {}".format(a, ls[i + 1], b, ls[i](a, b)))
