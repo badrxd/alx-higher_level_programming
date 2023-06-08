312059deee4a9033c23b275311caeb64960585c5
badrxd <badrxdxd@gmail.com>
2023-06-08 19:13:18 +0100

    ok

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    ls = [add, "+", sub, '-', mul, '*', div, '/']
+    for i in range(0, 8, 2):
+        print("{} {} {} = {}".format(a, ls[i - 1], b, ls[i](a, b)))
