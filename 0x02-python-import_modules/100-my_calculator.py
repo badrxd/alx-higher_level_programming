e46b4232a75ed8188d4445705357ce32626c76ea
badrxd <badrxdxd@gmail.com>
2023-06-09 01:30:28 +0100

    ok

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,38 +1,22 @@
 #!/usr/bin/python3
 
-def find_op(op):
-    ls_op = ['+', '-', '*', '/']
+def find_op(op, ls_op):
     if op not in ls_op:
         return True
     return False
 
 
-def result(a, op, b):
-
-    from calculator_1 import add, sub, mul, div
-
-    a = int(a)
-    b = int(b)
-
-    if op == '+':
-        return add(a, b)
-    elif op == '-':
-        return sub(a, b)
-    elif op == "'*'":
-        return mul(a, b)
-    else:
-        return div(a, b)
-
-
 if __name__ == "__main__":
     from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
 
-    a, op, b = argv[1], argv[2], argv[3]
+    a, op, b = int(argv[1]), str(argv[2]), int(argv[3])
+    ls_op = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    if find_op(op):
+    if find_op(op, ls_op):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(a, op, b, result(a, op, b)))
+    print("{} {} {} = {}".format(a, op, b, ls_op[op](a ,b)))
