24429c1c948835eff5fe77c92d8872c7dd9b8032
badrxd <badrxdxd@gmail.com>
2023-06-11 12:10:46 +0100

    ok

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not any(matrix):
+        print("")
+    for raw in matrix:
+        for i in range(0 ,len(raw)):
+            if i != (len(raw) - 1):
+                print('{}'.format(raw[i]), end=" ")
+            else:
+                print('{}'.format(raw[i]))
