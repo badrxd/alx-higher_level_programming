4f6fa6d4bc47d020f933450b232b9edc379af954
badrxd <badrxdxd@gmail.com>
2023-06-14 20:17:10 +0100

    ok

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    ls = []
+    for i in my_list:
+        if i == search : 
+            i = replace
+        ls.append(i)
+    return(ls)
