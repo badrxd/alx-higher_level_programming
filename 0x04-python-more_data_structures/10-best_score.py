827d1185c8ef7c4d6931d53a64942c819efb11e5
badrxd <badrxdxd@gmail.com>
2023-06-15 01:52:19 +0100

    ok

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    num  = 0
+    num = 0
     name = ""
     for key in a_dictionary:
         if a_dictionary[key] > num:
