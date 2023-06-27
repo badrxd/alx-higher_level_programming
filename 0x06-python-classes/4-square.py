6ceacd633f999842385abae5689a61732e944e84
badrxd <badrxdxd@gmail.com>
2023-06-27 16:59:39 +0100

    ok

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" define a class Square"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        x = self.__size ** 2
+        return x
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise TypeError("size must be >= 0")
+        else:
+            self.__size = value
