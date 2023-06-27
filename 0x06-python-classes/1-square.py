54aca2dfa19e31cc1a82a4dc2697f143e1d0c7ad
badrxd <badrxdxd@gmail.com>
2023-06-27 16:13:11 +0100

    ok

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" define a class Square"""
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Initialize class """
+        self.__size = size
