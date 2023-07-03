cd186f084329e9278c637a426b04e0289a9965e6
badrxd <badrxdxd@gmail.com>
2023-07-03 18:11:34 +0100

    ok

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,9 +4,12 @@
 
 
 class Rectangle():
-    number_of_instances = 0
+    """Represent a rectangle.
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+    """
 
-    """Represent a rectangle."""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """ the class Rectangle
