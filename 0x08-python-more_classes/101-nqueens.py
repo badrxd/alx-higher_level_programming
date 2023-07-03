7cba571b41bc935ba4ee4404038ef99d3015e766
badrxd <badrxdxd@gmail.com>
2023-07-03 23:27:03 +0100

    ok

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -122,17 +122,18 @@ def solve_recursive(board, row, queens, solutions):
     return (solutions)
 
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        sys.exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    board = board_init(int(sys.argv[1]))
-    solutions = solve_recursive(board, 0, 0, [])
-    for sol in solutions:
-        print(sol)
+
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    sys.exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+
+board = board_init(int(sys.argv[1]))
+solutions = solve_recursive(board, 0, 0, [])
+for sol in solutions:
+    print(sol)
