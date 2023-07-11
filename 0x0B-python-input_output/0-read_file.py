64e73e9a9c40a3867a437aa1bf691a34d30e89c2
badrxd <badrxdxd@gmail.com>
2023-07-11 18:05:34 +0100

    ok

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""function """
+
+
+def read_file(filename=""):
+    """ function that reads a text file
+    Args:
+        filename(file): file tha we dear from
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
