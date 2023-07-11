78a9c10ec6ddc7245899d8c53e06f3dd014cf001
badrxd <badrxdxd@gmail.com>
2023-07-11 18:30:18 +0100

    ok

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,9 +4,9 @@ import json
 
 
 def to_json_string(my_obj):
-    """ function that write in txt file
+    """ function that returns the JSON
+    representation of an object (string):
     Args:
         my_obj: data structure that need to be encoded
     """
-
-    return (json.dumps(my_obj))
+    return json.dumps(my_obj)
