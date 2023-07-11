5bc913fd7a067491ef70d4aa9e62463f6a9a5591
root <root@DESKTOP-J73E3I4.localdomain>
2023-07-11 20:21:48 +0100

    ok

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ import sys
 
 
 if __name__ == "__main__":
+    """function for saving to json"""
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
