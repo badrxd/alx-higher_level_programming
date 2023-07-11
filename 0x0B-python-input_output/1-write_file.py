39b63c7ab4c46b0d79c9f6c6dbde1441e95ed6c2
badrxd <badrxdxd@gmail.com>
2023-07-11 18:18:20 +0100

    ok

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""function """
+
+
+def write_file(filename="", text=""):
+    """ function that write in txt file
+    Args:
+        filename(txt): file where we will write
+        text(str): test that we will add to file txt
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
