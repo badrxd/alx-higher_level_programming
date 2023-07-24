12a43d1e19eedeb286691a9e0e07fbb6a02fcc7a
root <root@DESKTOP-J73E3I4.localdomain>
2023-07-24 20:11:13 +0100

    ok

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ Module represent class Base """
 import json
 import csv
+import turtle
 
 
 class Base:
@@ -134,3 +135,43 @@ class Base:
             return instances
         except FileNotFoundError:
             return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Drawing Rectangles and Squares.
+        Args:
+            list_rectangles (list): A list.
+            list_squares (list): A list.
+        """
+        turt = turtle.Turtle()
+        t.screen.bgcolor("#B4B4B4")
+        t.pensize(3)
+        t.shape("turtle")
+
+        t.color("#36FF33")
+        for rect in list_rectangles:
+            t.showturtle()
+            t.up()
+            t.goto(rect.x, rect.y)
+            t.down()
+            for i in range(2):
+                t.forward(rect.width)
+                t.left(90)
+                t.forward(rect.height)
+                t.left(90)
+            t.hideturtle()
+
+        t.color("#33FFC4")
+        for sq in list_squares:
+            t.showturtle()
+            t.up()
+            t.goto(sq.x, sq.y)
+            t.down()
+            for i in range(2):
+                t.forward(sq.width)
+                t.left(90)
+                t.forward(sq.height)
+                t.left(90)
+            t.hideturtle()
+
+        turtle.exitonclick()
