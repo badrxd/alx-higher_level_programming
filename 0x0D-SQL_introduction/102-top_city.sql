7571c14455fbddab3b17feaf34a30b78920d8a16
badrxd <badrxdxd@gmail.com>
2023-08-16 02:27:42 +0100

    ok

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- displays the top 3 of cities temperature during July and August ordered by temperature
+
+SELECT city, MAX(value) AS `avg_temp` FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
