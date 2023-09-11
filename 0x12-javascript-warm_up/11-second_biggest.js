c17efbfce96d2c8a6642f2f36a2f6c06ddfaf33a
badrxd <badrxdxd@gmail.com>
2023-09-11 22:11:56 +0100

    ok

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const argv = process.argv;
 const biger = [];
 if (argv.length <= 3) {
-  console.log("0");
+  console.log('0');
 } else {
   for (let i = 2; i < argv.length; i++) {
     biger[i - 2] = Number(argv[i]);
