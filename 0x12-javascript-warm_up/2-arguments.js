0090be0bca9bdd30b41e2d9d89e7f9e660889814
badrxd <badrxdxd@gmail.com>
2023-09-11 10:23:50 +0100

    ok

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length >= 4) {
+if (argv[3]) {
   console.log('Arguments found');
-} else if (argv.length >= 3) {
+} else if (argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
