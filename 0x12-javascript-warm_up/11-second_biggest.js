f33f110f57b6f3d916d4508d2fed6df5f1d78cb1
badrxd <badrxdxd@gmail.com>
2023-09-11 22:10:27 +0100

    ok

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 const argv = process.argv;
 const biger = [];
-for (let i = 2; i < argv.length; i++) {
-  biger[i - 2] = Number(argv[i]);
+if (argv.length <= 3) {
+  console.log("0");
+} else {
+  for (let i = 2; i < argv.length; i++) {
+    biger[i - 2] = Number(argv[i]);
+  }
+  biger.sort();
+  console.log(biger[biger.length - 2]);
 }
-biger.sort();
-console.log(biger[biger.length - 2]);
