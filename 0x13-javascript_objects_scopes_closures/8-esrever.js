bee7d565fcb24dab01d129dc349155e86f512e87
badrxd <badrxdxd@gmail.com>
2023-09-12 22:04:17 +0100

    ok

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  const List = [...list];
-  return List.reverse();
+  const List = [];
+  for (let i = list.length - 1; i > -1; i--) {
+    List[i - list.length] = list[i];
+  }
+  return list.reverse();
 };
