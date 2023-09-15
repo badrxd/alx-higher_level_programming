7d56f0436542bbcaf8a14585dc5cb19ab736f2a1
badrxd <badrxdxd@gmail.com>
2023-09-15 06:57:24 +0100

    ok

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-code that is safe from MySQL injections!
+a script that takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
 '''
 
 import sys
@@ -11,11 +12,10 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM\
-                cities LEFT JOIN states ON cities.state_id = states.id\
+    cur.execute("SELECT cities.name FROM cities WHERE cities.state_id\
+                = (SELECT id FROM states WHERE name = 'Texas')\
                 ORDER BY cities.id ASC")
     data = cur.fetchall()
-    for DATA in data:
-        print(DATA)
+    print(", ".join([name[0] for name in data]))
     cur.close()
     db.close()
