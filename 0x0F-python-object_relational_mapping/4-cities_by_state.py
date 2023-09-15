1e675ac14bac4f57d5372d87e003d9bfbfc611ea
badrxd <badrxdxd@gmail.com>
2023-09-15 06:09:45 +0100

    ok

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+code that is safe from MySQL injections!
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM\
+                cities LEFT JOIN states ON state_id = states.id\
+                ORDER BY cities.id ASC")
+    data = cur.fetchall()
+    for DATA in data:
+        print(DATA)
+    cur.close()
+    db.close()
