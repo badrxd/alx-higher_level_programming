710396f396b8b1ef4680d5f443fdb18653116075
root <root@DESKTOP-J73E3I4.localdomain>
2023-09-15 03:29:17 +0100

    ok

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-''' Write a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.'''
+'''
+code that is safe from MySQL injections!
+'''
 
 import sys
 import MySQLdb
@@ -10,7 +11,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name = %s", (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", (sys.argv[4],))
     data = cur.fetchall()
     for DATA in data:
         print(DATA)
