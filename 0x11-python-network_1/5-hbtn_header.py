58858fdc235ac783956404866de51addeaca53f1
badrxd <badrxdxd@gmail.com>
2023-09-29 16:19:05 +0100

    task 5. Response header value #1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """this code will not be executed if it was imported
+    """
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
