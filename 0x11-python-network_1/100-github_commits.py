f1f13b5e39eae20c3d329d317f61a230fe7470fc
badrxd <badrxdxd@gmail.com>
2023-09-29 22:11:58 +0100

    task 10. Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script that get the sha and the owner of the commit
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    """this code will not be executed if it was imported
+    """
+    if len(argv) > 1:
+        url = "https://api.github.com/repos/{}/{}/commits".format(
+            argv[1], argv[2])
+        try:
+            r = requests.get(url)
+            i = 0
+            for data in r.json():
+                owner = data['commit']['author']['name']
+                sha = data['sha']
+                print("{}: {}".format(sha, owner))
+                if i == 9:
+                    break
+                i += 1
+        except Exception:
+            pass
