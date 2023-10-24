8fed6dfe085dc5b397a7869d0ee55a500d94b3da
root <root@DESKTOP-J73E3I4>
2023-10-24 20:14:47 +0100

    task 5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,13 +5,13 @@ const args = process.argv;
 const request = require('request');
 
 request(args[2], function (err, body) {
-    if (err) {
-      console.error(err);
-    } else {
-        fs.writeFile(args[3], body.body, 'utf8', (err) => {
-            if (err) {
-              console.log(err);
-            }
-          });
-    }
-})
+  if (err) {
+    console.error(err);
+  } else {
+    fs.writeFile(args[3], body.body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
