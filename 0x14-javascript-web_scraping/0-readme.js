aee8599f21045029337dff0e2b26176399264008
badrxd <badrxdxd@gmail.com>
2023-10-24 18:20:36 +0100

    task 0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+const fs = require('fs');
+
+fs.readFile(`./${argv[2]}`, 'utf8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
