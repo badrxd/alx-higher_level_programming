c27c11376251de3011cdbd5a0b6a46abadd9d1b8
root <root@DESKTOP-J73E3I4>
2023-10-24 20:52:56 +0100

    task 6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,18 +3,16 @@
 const url = process.argv[2];
 const request = require('request');
 
-request(`${url}?completed=true`, function (err, body) {
+request(url, function (err, body) {
   if (err) {
     console.error(err);
   } else {
     const obj = {};
-
     const data = JSON.parse(body.body);
     data.forEach(element => {
-      if (obj[element.userId] === undefined) {
-        obj[element.userId] = 1;
-      } else {
-        obj[element.userId] = obj[element.userId] + 1;
+      if (element.completed) {
+        const i = element.userId;
+        obj[i] ? obj[i] = ++obj[i] : obj[i] = 1;
       }
     });
     console.log(obj);
