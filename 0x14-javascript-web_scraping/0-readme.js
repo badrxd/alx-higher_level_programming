ef181072d126cbda0d3f28c8e525c7a432b2df93
root <root@DESKTOP-J73E3I4>
2023-10-24 18:29:31 +0100

    task 0. Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 const args = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(`./${args}`, 'utf8', (err, data) => {
+fs.readFile(`${args}`, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
