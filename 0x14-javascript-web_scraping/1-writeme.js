c11e08c6e92a9774ddef4a0bc8a45b2635fac5e0
badrxd <badrxdxd@gmail.com>
2023-10-24 18:39:10 +0100

    task 1. Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const fs = require('fs');
 
-fs.writeFile(`${args[2]}`, `${args[3]}\n`,  'utf8', (err, data) => {
+fs.writeFile(`${args[2]}`, `${args[3]}\n`, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   }
